f56478197d4e77dc6f88e4d952b9cc47bc2a2050
ua007tr <63312194+ua007tr@users.noreply.github.com>
2020-04-27 20:16:00 +0530

    calculate the sum of digits

---
+++ sumofdigits.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+int sum (int);
+int main()
+{
+    int n, result;
+ 
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    result = sum(n);
+    printf("Sum of digits in %d is %d\n", n, result);
+    return 0;
+}
+ 
+int sum (int n)
+{
+    if (n != 0)
+    {
+        return (n % 10 + sum (n / 10));
+    }
+    else
+    {
+       return 0;
+    }
+}
